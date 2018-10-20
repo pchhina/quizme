16e862c2b2fb6f2fc38d33d96e98617bf18c7f92
Paramjot Singh <param.chhina@gmail.com>
2018-10-20 07:08:47 -0500

    stopping criteria implemented

--- R/user_functions.R
+++ R/user_functions.R
@@ -45,6 +45,13 @@ quizme <- function() {
         testlog <<- data_obj[[3]] 
         ranktbl <<- data_obj[[4]] 
     }
+    if(nrow(ranktbl)!= 0) {
+        testdue <<- ranktbl[1, due] # for subsetting session, see ask()
+        teststatus <<- ranktbl[1, status] # for subsetting session, see ask()
+    } else {
+        testdue <<- today()
+        teststatus <<- 'new'
+    }
 }
 
 #' Add question-answer
@@ -96,6 +103,8 @@ addq <- function(tags = c("")) {
 ask <- function() {
     if(nrow(qtbl) == 0) {
         cat("no questions exist yet \nplease use addq() to add questions\n")
+    } else if(nrow(ranktbl[due == testdue & status == teststatus]) == 0) {
+        cat("Finished Quiz!!! \nPlease come back at a later time to practice more.\n")
     } else {
     qid <<- ranktbl[1, id]
     timeasked <<- now()
